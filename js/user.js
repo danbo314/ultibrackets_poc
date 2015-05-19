33959c13bc9f8773a6a1fc8252c70f2de7e79288
Dan Bolivar <danbo314@gmail.com>
2015-05-18 21:36:57 -0400

    fix tpl issues

--- js/user.js
+++ js/user.js
@@ -7,13 +7,13 @@
     Parse.initialize("AxAhmixCD23l9oxpKc02kJewBmdDt5UQ159RB7ni", "Xoypi5WdNOe5V0xGALSUvvFlbAqwE8TeX89TkFeL");
 
     var currentUser = Parse.User.current();
-    console.log(currentUser);
+
     $.ajax({
         url: "../html/tpl/user.tpl",
         success: function (data) {
             var template = Handlebars.compile(data);
 
-            $("#test").html(currentUser);//template(args));
+            $("#test").html(template(currentUser.attributes));
         }
     });
 
